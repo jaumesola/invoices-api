cc9f294b07576e2b1c180b181ce495fdd2de4a35
consert <ask@jaumesola.com>
2017-09-20 11:43:58 +0200

    fix for staging support

--- server/api.js
+++ server/api.js
@@ -1,15 +1,19 @@
+if (Meteor.settings.public.environment == 'staging') {
+    Offers = new Mongo.Collection("offers");
+} else {
+    let mongoDbUrl    = "mongodb://localhost:3001/meteor";
+    let mongoOplogUrl = "mongodb://localhost:3001/local";
 
-let mongoDbUrl    = "mongodb://localhost:3001/meteor";
-let mongoOplogUrl = "mongodb://localhost:3001/local";
+    let driver = new MongoInternals.RemoteCollectionDriver(
+            mongoDbUrl, { oplogUrl: mongoOplogUrl }
+        );
 
-let driver = new MongoInternals.RemoteCollectionDriver(
-    mongoDbUrl, { oplogUrl: mongoOplogUrl }
-    );
-
-Offers = new Mongo.Collection("offers", {_driver: driver});
+    Offers = new Mongo.Collection("offers", {_driver: driver});
+}
 
 // Global API configuration
 var Api = new Restivus({
+    apiPath: 'v1/',
     useDefaultAuth: true,
     prettyJson: true
     });
